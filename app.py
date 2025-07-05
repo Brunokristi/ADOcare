5f2aea5ef38368aca67fa3a08fddc78d9a3e3be2
klipar <bhdnkarpenko123@gmail.com>
2025-07-05 11:55:55 +0200

    improved app file

--- app.py
+++ app.py
@@ -16,35 +16,26 @@ from routes.transport import transport_bp
 
 
 from utils.database import DATABASE_FILE, check_db
-import threading
-import webbrowser
-import os
-
-
-app = Flask(__name__)
-app.secret_key = "a3f8d3e87b5a4e5f9c6d4b2f6a1e8c3d"
-CORS(app)
-
-app.register_blueprint(main)
-app.register_blueprint(macro_bp)
-app.register_blueprint(car_bp)
-app.register_blueprint(company_bp)
-app.register_blueprint(nurse_bp)
-app.register_blueprint(doctor_bp)
-app.register_blueprint(diagnosis_bp)
-app.register_blueprint(patient_bp)
-app.register_blueprint(insurance_bp)
-app.register_blueprint(month_bp)
-app.register_blueprint(schedule_bp)
-app.register_blueprint(dekurz_bp)
-app.register_blueprint(transport_bp)
-
-
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000/")
 
 if __name__ == "__main__":
-    check_db()
-    threading.Timer(1.5, open_browser).start()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app = Flask(__name__)
+    app.secret_key = "a3f8d3e87b5a4e5f9c6d4b2f6a1e8c3d"
+    CORS(app)
 
+
+    app.register_blueprint(main)
+    app.register_blueprint(macro_bp)
+    app.register_blueprint(car_bp)
+    app.register_blueprint(company_bp)
+    app.register_blueprint(nurse_bp)
+    app.register_blueprint(doctor_bp)
+    app.register_blueprint(diagnosis_bp)
+    app.register_blueprint(patient_bp)
+    app.register_blueprint(insurance_bp)
+    app.register_blueprint(month_bp)
+    app.register_blueprint(schedule_bp)
+    app.register_blueprint(dekurz_bp)
+    app.register_blueprint(transport_bp)
+
+    check_db()
+    app.run(host="127.0.0.1", port=5000, debug=True)
