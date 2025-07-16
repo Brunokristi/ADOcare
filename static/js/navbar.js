8ef4e6a3fca18220e8cdca512f26d9ec2a13c56d
klipar <bhdnkarpenko123@gmail.com>
2025-07-16 21:56:42 +0300

    add key shortcuts to navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -14,3 +14,37 @@ function showMessage(msg) {
         }, 3000);
     }
 }
+
+// ===== set key shortcuts =====
+// Ctrl Z - back button
+// Ctrl D - domov (dashboard/
+// CTRL A - Ados
+// f1 - sestra
+// f2 - pacient
+// f3 - nastavenia
+// f4 - odhlasit sa
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && (event.key.toLowerCase() === 'z' || event.key.toLowerCase() === 'y')){
+        event.preventDefault();
+        history.back();
+    } else if (event.ctrlKey && event.key.toLowerCase() === 'd'){
+        event.preventDefault();
+        window.location.href = dashboardUrl;
+    } else if (event.ctrlKey && event.key.toLowerCase() === 'a'){
+        event.preventDefault();
+        window.location.href = adosUrl;
+    } else if (event.key === 'F1'){
+        event.preventDefault();
+        window.location.href = nurseUrl;
+    } else if (event.key === 'F2'){
+        event.preventDefault();
+        window.location.href = patientUrl;
+    } else if (event.key === 'F3'){
+        event.preventDefault();
+        window.location.href = settingsUrl;
+    } else if (event.key === 'F4'){
+        event.preventDefault();
+        window.location.href = logoutUrl;
+    }
+
+});
