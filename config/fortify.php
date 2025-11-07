e231f175b610f5a772fc10c9a06be5441e526594
Manthee1 <dinja.martin@gmail.com>
2025-11-07 21:35:39 +0100

    Remove unused Fortify actions, password validation rules, and Inertia configuration

--- config/fortify.php
+++ config/fortify.php
@@ -115,8 +115,6 @@ return [
     */
 
     'limiters' => [
-        'login' => 'login',
-        'two-factor' => 'two-factor',
     ],
 
     /*
@@ -144,14 +142,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
-        Features::resetPasswords(),
-        Features::emailVerification(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0
-        ]),
+
     ],
 
 ];
