04d8c24e7e202b00dd6e6d5fc7a9b8b2a5533f44
Manthee1 <dinja.martin@gmail.com>
2025-11-21 16:29:08 +0100

    Refactor PrimeVue integration and add custom theme configuration

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,57 +4,19 @@ import App from './App.vue';
 import router from '@/router';
 import api from '@/services/api';
 import useAuthStore from './stores/auth';
-import Aura from '@primeuix/themes/aura';
 
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Checkbox from 'primevue/checkbox';
 
+import primeVueConfig from './config/primeVueConfig';
+import "primeicons/primeicons.css"
 
 const app = createApp(App);
 const pinia = createPinia();
 
 
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkModeSelector: '.my-app-dark',
-            cssLayer: false
-        },
-    },
-    pt: {
-        datatable: {
-            root: { class: 'rounded-md overflow-hidden' },
-            table: { class: 'w-full border-collapse !border-0' },
-            bodyrow: { class: '[&.p-row-odd]:!bg-almostwhite [&.p-row-odd:hover]:!bg-lightgrey hover:!bg-lightgrey' },
-        },
-
-        column: {
-            headercell: { class: '!bg-darkgrey !text-white !text-heading px-4 py-2 !border-r !border-r-white last:!border-r-0' },
-            sortIcon: { class: '!text-white !flex !items-center !justify-center' },
-            pcheadercheckbox: {
-                box: { class: ' !bg-darkgrey !border-white' },
-            },
-            pcrowcheckbox: {
-                box: {
-                    class: '!border-darkgrey custom-checkbox-fill'
-                },
-            },
-            bodycell: {
-                class: '!text-normal !text-darkgrey !p-0 !px-4 !border-0 !border-r !border-r-white last:!border-r-0',
-            },
-
-        },
-        inputtext: {
-            root: { class: ' !border-darkgrey !text-darkgrey' }
-        },
-    }
-});
-
-
+app.use(PrimeVue, primeVueConfig);
 app.use(pinia);
 app.use(router);
 app.use(ToastService);
---
+++ resources/js/config/primeVueConfig.ts
@@ -0,0 +1,62 @@
+import Material from '@primeuix/themes/material';
+
+import type { PrimeVueConfiguration } from 'primevue/config';
+import { definePreset } from '@primeuix/themes';
+
+const ADOPreset = definePreset(Material, {
+    semantic: {
+        colorScheme: {
+            light: {
+                formFieldPadding: {
+                    y: '0.2rem',
+                    x: '0.5rem',
+                },
+                listOptionPadding: "0.2rem 0.5rem",
+            }
+        }
+    }
+});
+
+
+export default {
+    theme: {
+        preset: ADOPreset,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.dark-theme',
+            cssLayer: false
+
+        },
+    },
+
+    pt: {
+        selectbutton: {
+
+        },
+        datatable: {
+            root: { class: 'rounded-md overflow-hidden' },
+            table: { class: 'w-full border-collapse !border-0' },
+            bodyrow: { class: '[&.p-row-odd]:!bg-almostwhite [&.p-row-odd:hover]:!bg-lightgrey hover:!bg-lightgrey' },
+        },
+
+        column: {
+            headercell: { class: '!bg-darkgrey !text-white !text-heading px-4 py-2 !border-r !border-r-white last:!border-r-0' },
+            sortIcon: { class: '!text-white !flex !items-center !justify-center' },
+            pcheadercheckbox: {
+                box: { class: ' !bg-darkgrey !border-white' },
+            },
+            pcrowcheckbox: {
+                box: {
+                    class: '!border-darkgrey custom-checkbox-fill'
+                },
+            },
+            bodycell: {
+                class: '!text-normal !text-darkgrey !p-0 !px-4 !border-0 !border-r !border-r-white last:!border-r-0',
+            },
+
+        },
+        inputtext: {
+            root: { class: ' !border-darkgrey !text-darkgrey' }
+        },
+    }
+} as PrimeVueConfiguration;
