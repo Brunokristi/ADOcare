4f189ad1b7062d9ee89aa9032f3965001d29d249
Manthee1 <dinja.martin@gmail.com>
2025-12-11 22:50:33 +0100

    Add patients relationship to Branch model and update PatientSeeder to generate patients per branch

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
 use App\Models\Patient;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +10,20 @@ class PatientSeeder extends Seeder
 {
     public function run(): void
     {
-        Patient::factory(50)->create();
+
+        // For each branch generate multiple patients
+        $branches = Branch::all();
+
+        $branches->each(function (Branch $branch) {
+            $patients = Patient::factory()->count(random_int(20,60))->create();
+            $branch->users()->each(function ($user) use ($patients, $branch) {
+                // Attach each patient to the user with the branch pivot
+                foreach ($patients as $patient) {
+                    $patient->assignedUsers()->attach($user->id, ['branch_id' => $branch->id]);
+                }
+            });
+
+
+        });
     }
 }
