6f41eb243b0414c1fd388ba13a481270048c89f2
Manthee1 <dinja.martin@gmail.com>
2025-12-12 18:43:41 +0100

    Remove deprecated initial schema migration and add new branch pivots migration

---
+++ database/migrations/2025_11_07_000011_create_branch_pivots.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserMacrosDekurz extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+    }
+}
