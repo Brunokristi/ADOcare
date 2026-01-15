4e4436528da76ce57c2e2eb5485c00a9a1f86d98
Manthee1 <dinja.martin@gmail.com>
2026-01-15 09:41:32 +0100

    Enhance Visit model and factory: add user and branch associations, and new time metrics

--- database/factories/VisitFactory.php
+++ database/factories/VisitFactory.php
@@ -4,7 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Visit;
 use App\Models\Patient;
-use App\Models\ReportMonth;
+use App\Models\User;
+use App\Models\Branch;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VisitFactory extends Factory
@@ -15,10 +16,16 @@ class VisitFactory extends Factory
     {
         return [
             'date' => $this->faker->dateTimeBetween('-1 years', 'now')->format('Y-m-d'),
-            'examination' => $this->faker->dateTimeBetween('-1 years', 'now')->format('Y-m-d H:i:s'),
-            'statement' => $this->faker->dateTimeBetween('-1 years', 'now')->format('Y-m-d H:i:s'),
             'patient_id' => Patient::inRandomOrder()->value('id') ?? null,
-            'month_id' => ReportMonth::inRandomOrder()->value('id') ?? null,
+            // Associate to an existing user and branch when available
+            'user_id' => User::inRandomOrder()->value('id') ?? null,
+            'branch_id' => Branch::inRandomOrder()->value('id') ?? null,
+            // Times: terrain_time and administrative_time are stored as timestamps
+            'terrain_time' => $this->faker->dateTimeBetween('-1 years', 'now')->format('Y-m-d H:i:s'),
+            'administrative_time' => $this->faker->dateTimeBetween('-1 years', 'now')->format('Y-m-d H:i:s'),
+            'time_on_location' => $this->faker->numberBetween(60, 7200),
+            'distance_to_location' => $this->faker->numberBetween(100, 50000),
+            'time_to_location' => $this->faker->numberBetween(60, 3600),
         ];
     }
 }
