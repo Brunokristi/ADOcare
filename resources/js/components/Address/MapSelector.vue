150524aef41009e985fbf942daec91700cbd50fe
Manthee1 <dinja.martin@gmail.com>
2026-02-06 20:37:50 +0100

    FEAT: enhance AddressAutocomplete and MapSelector components with improved prop handling and logging

--- resources/js/components/Address/MapSelector.vue
+++ resources/js/components/Address/MapSelector.vue
@@ -2,23 +2,28 @@
 import { computed, ref, watch } from 'vue'
 import { LMap, LMarker, LTileLayer } from '@vue-leaflet/vue-leaflet'
 import type { PointExpression } from 'leaflet'
+import { onMounted } from 'vue';
 
 const props = defineProps<{ latitude?: number | null; longitude?: number | null }>()
+const zoom = ref(13)
+
 const emit = defineEmits<{
     (e: 'update', payload: { lat: number | null; lon: number | null }): void
 }>();
 
 const center = computed<PointExpression>(() => {
     if (props.latitude && props.longitude) {
+
+        console.log('centering map to', props.latitude, props.longitude);
         return [props.latitude, props.longitude]
     }
+    console.log('centering map to Bratislava as default');
     return [48.1486, 17.1077]
 })
-const zoom = ref(13)
-
-
 
 watch(() => [props.latitude, props.longitude], ([lat, lon]) => {
+    console.log('props changed, new center:', lat, lon);
+
     if (lat && lon) {
         zoom.value = 15
     }
@@ -29,11 +34,21 @@ function onMapClick(e: any) {
     const lon = e.latlng?.lng ?? null
     emit('update', { lat, lon })
 }
+const lmapKey = ref('lmap-' + Math.random().toString(36).substring(2, 9))
+onMounted(() => {
+    setTimeout(() => {
+        lmapKey.value = 'lmap-' + Math.random().toString(36).substring(2, 9)
+    }, 100)
+})
+
+
 </script>
+
 
 <template>
     <div class="h-64 rounded-md overflow-hidden">
-        <LMap :center="center" :zoom="zoom" :useGlobalLeaflet="false" style="height:100%" @click="onMapClick">
+        <LMap :key="lmapKey" :center="center" :zoom="zoom" :useGlobalLeaflet="false" style="height:100%"
+            @click="onMapClick">
             <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <LMarker v-if="props.latitude && props.longitude" :lat-lng="[props.latitude, props.longitude]" />
         </LMap>
