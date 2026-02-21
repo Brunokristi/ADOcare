8c782dea8a118ed76c80dae48e4e7b6454ef8d51
Bruno <brunokristian003@gmail.com>
2026-02-21 08:43:33 +0100

    removed old adocare

--- adocare-flask/models/macro.py
+++
@@ -1,7 +0,0 @@
-class Macro:
-    def __init__(self, row):
-        self.id = row['id']
-        self.nazov = row['nazov']
-        self.text = row['text']
-        self.skratka = row['skratka']
-        self.farba = row['farba']
--- adocare-flask/wsgi.py
+++
@@ -1,3 +0,0 @@
-from core import create_app
-app = create_app()
-# gunicorn wsgi:app -b 0.0.0.0:8000 --workers 3
